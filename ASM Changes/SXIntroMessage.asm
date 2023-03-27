930ac541ff07a0e6a4e9c1b17d2181f947149f29
blazinzzetti <24198652+BlazinZzetti@users.noreply.github.com>
2023-03-26 17:24:29 -0700

    Fix file that was not saved.

--- ASM Changes/SXIntroMessage.asm
+++ ASM Changes/SXIntroMessage.asm
@@ -1,4 +1,4 @@
-#To be inserted at 8035a280
+#To be inserted at 8035a284
 ;Boot intro SX
 
 ;Load Intro Message ID (8057D8FD)
@@ -14,4 +14,4 @@ li r17, 0x0
 li r18, 0x0
 
 ;Original code
-;lhz r4, 40
+li r5, 0
