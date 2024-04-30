673a4ef3911785fccd9f9e818921fcf9cb1be086
BlazinZzetti <24198652+BlazinZzetti@users.noreply.github.com>
2024-04-29 22:46:26 -0700

    Move Default Save Settings check to Render.
    This ensures the data is updated after save data is loaded in, automatically updating older save files.

--- ASM Changes/SXIntroMessageRender.asm
+++ ASM Changes/SXIntroMessageRender.asm
@@ -43,25 +43,100 @@ RenderSXIntro:
   b RenderSXIntro
 
 RenderSXStart:
-  ;r10 contains address to start of our message.
-  lwz r21, 0(r10)
-
-  ;Start by rendering a blank screen
-  mr r20, r21
-  
-  bl GetNewButtonPressesToR16
-
-  andi. r18, r16, 32
-  ;r18 is z button press isolated.
-  
-  cmpwi r18, 32
-  bne StoreCurrentInputAndExit
-
-  ;Set Screen to Options if Z is pressed.
-  li r19, 3
+  ;First, ensure save data values are set up.
+  ;Set r18 to start of flag data.
   lis r18, 0x8057
-  ori r18, r18, 0xD8FA
-  sth r19, 0(r18)
+  ori r18, r18, 0x7B2C
+
+  CheckCSSkip:
+    ;Load location for CSSkip flag
+    lbz r19, 0(r18)
+
+    ;If not 0, or 1
+    ;assign 1 by default.
+    cmpwi r19, 0
+    beq CheckRT
+    cmpwi r19, 1
+    beq CheckRT
+    li r19, 1
+    stb r19, 0(r18)
+
+  CheckRT:
+    ;Load location for Race Time flag
+    lbz r19, 1(r18)
+
+    ;If not 0, or 1
+    ;assign 0 by default.
+
+    cmpwi r19, 0
+    beq CheckMUI
+    cmpwi r19, 1
+    beq CheckMUI
+    li r19, 0
+    stb r19, 1(r18)
+
+  CheckMUI:
+    ;Load location for Modern UI Control flag
+    lbz r19, 2(r18)
+
+    ;If not 0, or 1
+    ;assign 0 by default.
+
+    cmpwi r19, 0
+    beq CheckSPWDisable
+    cmpwi r19, 1
+    beq CheckSPWDisable
+    li r19, 0
+    stb r19, 2(r18)
+
+  CheckSPWDisable:
+    ;Load location for Disable SPW Unlock flag
+    lbz r19, 3(r18)
+
+    ;If not 0, or 1
+    ;assign 0 by default.
+
+    cmpwi r19, 0
+    beq CheckKeyDisable
+    cmpwi r19, 1
+    beq CheckKeyDisable
+    li r19, 0
+    stb r19, 3(r18)
+
+  CheckKeyDisable:
+    ;Load location for Disable SPW Unlock flag
+    lbz r19, 4(r18)
+
+    ;If not 0, or 1
+    ;assign 0 by default.
+
+    cmpwi r19, 0
+    beq StartSXIntroRender
+    cmpwi r19, 1
+    beq StartSXIntroRender
+    li r19, 0
+    stb r19, 4(r18)
+
+  StartSXIntroRender:
+    ;r10 contains address to start of our message.
+    lwz r21, 0(r10)
+
+    ;Start by rendering a blank screen
+    mr r20, r21
+  
+    bl GetNewButtonPressesToR16
+
+    andi. r18, r16, 32
+    ;r18 is z button press isolated.
+  
+    cmpwi r18, 32
+    bne StoreCurrentInputAndExit
+
+    ;Set Screen to Options if Z is pressed.
+    li r19, 3
+    lis r18, 0x8057
+    ori r18, r18, 0xD8FA
+    sth r19, 0(r18)
  
   b StoreCurrentInputAndExit
 
