6d59294ae5b99aa8592e38093cd04aadce97ce00
BlazinZzetti <24198652+BlazinZzetti@users.noreply.github.com>
2024-06-19 20:34:00 -0700

    Egg Dealer IGT Updates
    These files will prevent the timer from hiding during attacks, and apply an IGT penalty when viewing a cutscene more than once if in Story Mode.

---
+++ ASM Changes/SetPreventTimeStopEggDealerAttack.asm
@@ -0,0 +1,137 @@
+#To be inserted at 80312190
+;SetPreventTimeStopEggDealerAttack.asm
+Start:
+  ;Set Flag to prevent HUD Hide and Time Stop.
+  ;The Flag will be cleared once noticed by the Check.asm
+  ;Borrowing In Checkpoint Flag for preventing Timer Stop.
+
+  cmpwi r4, 0 ;0 = Intro
+  beq IntroCheck
+  cmpwi r4, 1 ;1 = Mid
+  beq MidCheck
+  cmpwi r4, 2 ;2 = Fight End
+  beq FightEndCleanup
+  cmpwi r4, 6 ;Missiles (Intro Attack)
+  beq End 
+
+  ;Check the Stage Action for if we are actively dying
+  ;80575F80
+  lis r18, 0x8057
+  ori r18, r18, 0x5F80
+  lwz r18, 0(r18)
+  cmpwi r18, 9
+  beq End
+
+  ;Load value of "In Checkpoint" into r16
+  lis r18, 0x8057
+  ori r18, r18, 0xD8A6
+  li r17, 1
+  sth r17, 0(r18)
+  b End
+
+;8057D900 (normally show time on run end) will be used to keep track of Intro and Mid Shown.
+;8057D900 = Intro
+;8057D901 = Mid
+
+;8057D902 = Storage for plenalty time?
+
+;8057D734 = Timer as float
+
+IntroCheck:
+  lis r18, 0x8057
+  ori r18, r18, 0xD900
+  lbz r18, 0(r18)
+  cmpwi r18, 0
+  beq IntroSet
+
+  ;If Intro Seen Again, Add 14.54 seconds 0x4168A3D7
+  lis r18, 0x8057
+  ori r18, r18, 0xD902
+
+  lis r17, 0x4168
+  ori r17, r17, 0xA3D7
+  stw r17, 0(r18)
+  lfs f0, 0(r18)
+
+  lis r18, 0x8057
+  ori r18, r18, 0xD734
+  lfs f1, 0x1D4(r18)
+  fadds f0, f0, f1
+  stfs f0, 0x1D4(r18)  
+
+IntroSet:
+  ;Load "StageSequenceManager Phase" into r16.
+  ;Dont Set a value if not Story Mode
+  lis r16, 0x805E
+  ori r16, r16, 0xF9A8
+  lwz r16, 0x0(r16)
+  lwz r16, 0x4(r16)
+  cmpwi r16, 1
+  bne End
+
+  lis r18, 0x8057
+  ori r18, r18, 0xD900
+  li r17, 1
+  stb r17, 0(r18)
+  b Cleanup
+
+MidCheck:
+  lis r18, 0x8057
+  ori r18, r18, 0xD901
+  lbz r18, 0(r18)
+  cmpwi r18, 0
+  beq MidSet
+
+  ;If Mid Seen Again, Add 1.99 seconds 0x3FFEB852
+  lis r18, 0x8057
+  ori r18, r18, 0xD902
+
+  lis r17, 0x3FFE
+  ori r17, r17, 0xB852
+  stw r17, 0(r18)
+  lfs f0, 0(r18)
+
+  lis r18, 0x8057
+  ori r18, r18, 0xD734
+  lfs f1, 0x1D4(r18)
+  fadds f0, f0, f1
+  stfs f0, 0x1D4(r18)
+
+MidSet:
+  ;Load "StageSequenceManager Phase" into r16.
+  ;Dont Set a value if not Story Mode
+  lis r16, 0x805E
+  ori r16, r16, 0xF9A8
+  lwz r16, 0x0(r16)
+  lwz r16, 0x4(r16)
+  cmpwi r16, 1
+  bne End
+
+  lis r18, 0x8057
+  ori r18, r18, 0xD901
+  li r17, 1
+  stb r17, 0(r18)
+
+Cleanup:
+  ;Restore Memory and Registries
+  lis r18, 0x8057
+  ori r18, r18, 0xD902
+  li r17, 0
+  stw r17, 0(r18)
+  lfs f0, 0(r18)
+  lfs f1, 0(r18)
+  b End
+
+FightEndCleanup:
+  lis r18, 0x8057
+  ori r18, r18, 0xD900
+  li r17, 0
+  sth r17, 0(r18)
+  stw r17, 2(r18)
+
+End:
+  ;Original Code
+  li r4, 4
+
+
+
---
+++ ASM Changes/SetShowRaceTimeFalseOnPauseExit.asm
@@ -0,0 +1,17 @@
+#To be inserted at 801e33dc
+;SetShowRaceTimeFalseOnPauseExit.asm
+;To account for Boss Battle usage.
+
+Start:
+  li r5, 0
+  ;Set "Show Race Time" to false.
+  lis r18, 0x8057
+  ori r18, r18, 0xD900
+  sth r5, 0(r18)
+
+  ;Original Code 
+  li r5, 1
+
+  ;Cleanup to prevent
+  ;accidental code changes
+  li r18, 0x0
