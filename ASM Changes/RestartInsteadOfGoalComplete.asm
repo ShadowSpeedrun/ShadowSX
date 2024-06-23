aabc546d8abb330ccacf65c7b99f6f90945d93f2
BlazinZzetti <24198652+BlazinZzetti@users.noreply.github.com>
2024-06-22 20:51:55 -0700

    Initial File for Restart on Goal Complete

---
+++ ASM Changes/RestartInsteadOfGoalComplete.asm
@@ -0,0 +1,11 @@
+#To be inserted at 8016b08c
+;RestartInsteadOfGoalComplete.asm
+
+;If Practice Mode, Restart instead 
+;of Mission Complete to allow faster practice
+;without worry of restarting right before.
+;Added benefit of preventing new invalid times.
+
+;Original Code
+li r4, 6
+li r4, 7
