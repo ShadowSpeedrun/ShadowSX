63cd596f669079c0b874e8f668b8a362eb48e4dd
BlazinZzetti <24198652+BlazinZzetti@users.noreply.github.com>
2024-06-24 22:01:16 -0700

    Update RestartInsteadOfGoalComplete.asm

--- ASM Changes/RestartInsteadOfGoalComplete.asm
+++ ASM Changes/RestartInsteadOfGoalComplete.asm
@@ -1,11 +1,24 @@
-#To be inserted at 8016b08c
-;RestartInsteadOfGoalComplete.asm
+#8016b08c
+#RestartInsteadOfGoalComplete.asm
 
-;If Practice Mode, Restart instead 
-;of Mission Complete to allow faster practice
-;without worry of restarting right before.
-;Added benefit of preventing new invalid times.
+#If Practice Mode, Restart instead 
+#of Mission Complete to allow faster practice
+#without worry of restarting right before.
+#Added benefit of preventing new invalid times.
 
-;Original Code
-li r4, 6
-li r4, 7
+#Practice Mode Flag = 8057D8F4
+
+Start:
+  #Original Code
+  li r4, 6
+
+  #If Practice Mode, Restart instead.
+  lis r18, 0x8057
+  ori r18, r18, 0xD8F4
+  lbz r18, 0(r18)
+  cmpwi r18, 1
+  bne End
+
+  #Change Action
+  li r4, 7
+End:
