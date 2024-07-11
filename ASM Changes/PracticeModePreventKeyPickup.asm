76a200bbb2fbbabc113598bd19a2456d8076a21e
BlazinZzetti <24198652+BlazinZzetti@users.noreply.github.com>
2024-07-10 21:52:15 -0700

    Finish Keys Available in Practice Mode

--- ASM Changes/PracticeModePreventKeyPickup.asm
+++ ASM Changes/PracticeModePreventKeyPickup.asm
@@ -1,4 +1,4 @@
-#8011bef0
+#8011c244
 #PracticeModePreventKeyPickup.asm
 
 #Check if Practice Mode (8057D8F4)
@@ -6,13 +6,13 @@ lis r16, 0x8057
 ori r16, r16, 0xD8F4
 lbz r16, 0(r16)
 cmpwi r16, 1
-beq PreventSave
+beq ForceLoad
 
 #Original Code
-stwx r3, r5, r4
+lwzx r0, r3, r4
 b End
 
-PreventSave:
+ForceLoad:
   li r0, 100
 
 End:
