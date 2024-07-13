09c815085357051ecba66351f4c13014326a0214
BlazinZzetti <24198652+BlazinZzetti@users.noreply.github.com>
2024-07-12 19:14:49 -0700

    Intro Options Fixes for other Langs
    Needs new .mets

--- ASM Changes/SXIntroMessageSetup.asm
+++ ASM Changes/SXIntroMessageSetup.asm
@@ -10,28 +10,61 @@ Start:
   #Load Intro Message ID (8057D8FC)
   lis r18, 0x8057
   ori r18, r18, 0xD8FC
-  lwz r19, 0(r18)
-  cmplwi r19, 0x0
-  bne- End
+  lhz r19, 0(r18)
   
+  #If 0, run init
+  cmplwi r19, 0x0
+  beq Init
+  
+  #Check if we are past Intro into Options
+  lhz r19, -2(r18)
+  cmpwi r19, 0x0
+  bgt InitOptionSets
+  b End
+
+Init:  
   #Initialize to SX intro
   li r19, 44
   sth r19, 0(r18)
 
+  #Check if Rom Settings are valid
+  #If not, assign default settings.
+
+  #Set r18 to start of flag data.
+  lis r18, 0x8057
+  ori r18, r18, 0x7B2C
+
+InitMenuOptions:
+  #8057FB80 is current level keys. Borrowing for lookup table data.
+  lis r16, 0x8057
+  ori r18, r16, 0xFBA0
+
+  #Set the values for Page 2 to be "No Change" by default
+  #07, 07, 07, 02
+  li r16, 0x0707
+  sth r16, 0(r18)
+  li r16, 0x0702
+  sth r16, 2(r18)
+  b End
+
+InitOptionSets:
   #Initialize Options References
   lis r16, 0x8057
   addi r18, r16, 0x6972
   lhz r16, 0(r18)
-  cmpwi r16, 0 #0 == JPN Lang
+  cmpwi r16, 1 #0 == JPN Lang
+               #1 == ENG Lang
   
   #8057FB80 is current level keys. Borrowing for lookup table data.
   lis r16, 0x8057
   ori r18, r16, 0xFB80
-  beql Set2Offsets
-  
-  #Not JPN Offsets
-  #38, 78, B8, F8, 138
-  li r17, 10
+  li r17, 10 #Offset for Set 1
+  ble OptionSets
+  li r17, 8 #Offsets for Set 2
+ 
+OptionSets: 
+  #Set 1 Offsets
+  #0038, 0078, 00B8, 00F8, 0138
 
   li r16, 0x0038
   add r16, r16, r17
@@ -53,7 +86,7 @@ Start:
   add r16, r16, r17
   sth r16, 8(r18)
 
-  #0062, 00A2, 00E2, 0122, 162
+  #0062, 00A2, 00E2, 0122, 0162
   li r16, 0x0062
   add r16, r16, r17
   sth r16, 10(r18)
@@ -73,39 +106,6 @@ Start:
   li r16, 0x0162
   add r16, r16, r17
   sth r16, 18(r18)
-
-  b InitOptionDefaults
-
-Set2Offsets:
-  #0038, 006A, 00A2, 0000
-  li r16, 0x0038
-  sth r16, 4(r18)
-  li r16, 0x006A
-  sth r16, 6(r18)
-  li r16, 0x00A2
-  sth r16, 8(r18)
-  li r16, 0x0000
-  sth r16, 10(r18)
-
-InitOptionDefaults:  
-  #Check if Rom Settings are valid
-  #If not, assign default settings.
-
-  #Set r18 to start of flag data.
-  lis r18, 0x8057
-  ori r18, r18, 0x7B2C
-
-InitMenuOptions:
-  #8057FB80 is current level keys. Borrowing for lookup table data.
-  lis r16, 0x8057
-  ori r18, r16, 0xFBA0
-
-  #Set the values for Page 2 to be "No Change" by default
-  #07, 07, 07, 02
-  li r16, 0x0707
-  sth r16, 0(r18)
-  li r16, 0x0702
-  sth r16, 2(r18)
 
 End:
   li r16, 0x0
