7dc29a50104eaddeb2968d0214dc974470058a71
BlazinZzetti <24198652+BlazinZzetti@users.noreply.github.com>
2024-07-16 21:31:47 -0700

    Restore updated FNTOverrideRedirect.asm
    Might have been lost during branch merges?

--- ASM Changes/FNTOverrideRedirect.asm
+++ ASM Changes/FNTOverrideRedirect.asm
@@ -8,7 +8,20 @@ Start:
   #TODO: Confirm this is checking for OK prompt
   cmpwi r11, 1
   bne End
-  
+
+  #Load Current Message ID
+  lis r18, 0x807D
+  addi r18, r18, 0x5700
+  lwz r16, 0(r18)
+  li r18, 0
+
+  #Only apply offset if it's a valid Message ID
+  cmpwi r16, 43
+  beq ApplyOffset
+  cmpwi r16, 44
+  bne End
+
+ApplyOffset:
   #Get Custom Offset.
   #If above 0, assume we want to show a custom message.
   lis r18, 0x8057
@@ -17,7 +30,7 @@ Start:
 
   #r18 is offset to use in actual bytes.
   mulli r18, r16, 0x14
-  
+
 End:
   #Original Code
   #lwz r3, 0 (r10)
@@ -25,6 +38,3 @@ End:
   lwzx r3, r10, r18 
   li r16, 0
   li r18, 0
-
-
-
