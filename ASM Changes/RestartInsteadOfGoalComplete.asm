e124c2846d915c2565b92460d17c559918dc6c80
BlazinZzetti <24198652+BlazinZzetti@users.noreply.github.com>
2024-08-22 22:06:47 -0700

    Practice Mode - CCG Fix + Add Missing Cases

--- ASM Changes/RestartInsteadOfGoalComplete.asm
+++ ASM Changes/RestartInsteadOfGoalComplete.asm
@@ -20,12 +20,17 @@ Start:
   #If Practice Mode, Restart instead.
   lis r18, 0x8057
   ori r18, r18, 0xD8F4
-  lbz r18, 0(r18)
-  cmpwi r18, 1
+  lbz r17, 0(r18)
+  cmpwi r17, 1
   bne End
 
   #Change Action
   li r4, 7
+  
+  #Set "In Checkpoint" to false in case of CCG
+  li r17, 0
+  sth r17, -0x4E(r18)
+
 End:
 
   #Manage Stack Pointer and Return
